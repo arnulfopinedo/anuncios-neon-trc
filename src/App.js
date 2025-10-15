d73a5a9e175a044d89391c784f99764fc60110c7
arnulfopinedo <arnulfo.angel2017@gmail.com>
2025-10-14 23:30:35 -0600

    Modificando archivos

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React desde Github
+          Aprende React desde Github
         </a>
       </header>
     </div>
